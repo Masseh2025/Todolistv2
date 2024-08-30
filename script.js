7789cd3cf487e1375a85c24bbfef1edc18911432
mas <mas154572@gmail.com>
2024-08-30 19:50:44 -0400

    added local storage functionality

---
+++ script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+// selecting elements
+const list = document.querySelector("[data-list]");
+const taskInput = document.querySelector("#task-input");
+// saving data for local storage
+let dataList = [];
+let dataString;
+
+taskInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    if (taskInput.value === "") return;
+    createTask(taskInput.value);
+    taskInput.value = "";
+  }
+});
+
+function createUniqueId() {
+  const id = new Date().getTime();
+  return id;
+}
+
+function createTask(value) {
+  //   creating the actual task
+
+  const task = document.createElement("li");
+  const taskContent = (task.innerText = value);
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  task.prepend(checkbox);
+  task.classList.add("todo-list__task");
+  const id = (task.dataset.taskid = createUniqueId());
+  //   convert to object
+  const taskData = {
+    id,
+    taskContent,
+  };
+
+  saveTask(taskData);
+
+  //   appending it to list
+  list.append(task);
+}
+
+function saveTask(data) {
+  dataList.push(data);
+  dataString = JSON.stringify(dataList);
+  localStorage.setItem("taskData", dataString);
+}
+
+function deleteTask() {
+  console.log("hello");
+}
+
+function uploadData() {
+  const parsedData = JSON.parse(localStorage.taskData);
+  dataList = parsedData;
+  for (let { id, taskContent } of parsedData) {
+    const task = document.createElement("li");
+    task.innerText = taskContent;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    task.prepend(checkbox);
+    task.classList.add("todo-list__task");
+    list.append(task);
+  }
+}
+
+uploadData();

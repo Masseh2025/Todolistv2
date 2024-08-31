9c7d2fc1f15907628be46f0a97ee1a549c60715e
mas <mas154572@gmail.com>
2024-08-31 02:25:16 -0400

    Created init function and added comments
    Basicly done

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const taskInput = document.querySelector("#task-input");
 let dataList = [];
 let dataString;
 
+// adding event lister to the task input
 taskInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     if (taskInput.value === "") return;
@@ -15,41 +16,55 @@ taskInput.addEventListener("keypress", (e) => {
   }
 });
 
+// this function makes a unique value for when the task turns into a object for json
 function createUniqueId() {
   const id = new Date().getTime();
   return id;
 }
 
-function init() {
-  console.log("hiu");
+// this function helps creates the elements "checkbox" and "delete button"
+function init(create) {
+  if (create === "checkbox") {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", check);
+    return checkbox;
+  }
+  if (create === "delete button") {
+    const deleteButton = document.createElement("div");
+    deleteButton.classList.add("todo-list__delete-button");
+    deleteButton.addEventListener("click", deleteTask);
+    return deleteButton;
+  }
 }
 
-function createTask(value) {
-  //   creating the actual task
+// When a checkbox is checked it will toggle the stroke class
+function check() {
+  console.log(this.parentElement.classList.toggle("todo-list__task--stroke"));
+}
 
+// this function creates the acutal task
+function createTask(value) {
   const task = document.createElement("li");
   const taskContent = (task.innerText = value);
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  task.prepend(checkbox);
-  const deleteButton = document.createElement("div");
-  deleteButton.classList.add("todo-list__delete-button");
-  deleteButton.addEventListener("click", deleteTask);
-  task.append(deleteButton);
+  task.prepend(init("checkbox"));
+  task.append(init("delete button"));
   task.classList.add("todo-list__task");
   const id = (task.dataset.taskid = createUniqueId());
-  //   convert to object
+  //   convert to object for json
   const taskData = {
     id,
     taskContent,
   };
 
+  //   saves the task
   saveTask(taskData);
 
   //   appending it to list
   list.append(task);
 }
 
+// saves the task
 function saveTask(data) {
   if (data) {
     dataList.push(data);
@@ -58,6 +73,7 @@ function saveTask(data) {
   localStorage.setItem("taskData", dataString);
 }
 
+// This function deletes the task from the dom and datalist
 function deleteTask() {
   const deleteId = Number(this.parentElement.dataset.taskid);
   console.log(deleteId);
@@ -68,19 +84,15 @@ function deleteTask() {
   saveTask();
 }
 
+// This function creates the elements from a previous session from local storage
 function uploadData() {
   const parsedData = JSON.parse(localStorage.taskData);
   dataList = parsedData;
   for (let { id, taskContent } of parsedData) {
     const task = document.createElement("li");
     task.innerText = taskContent;
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    task.prepend(checkbox);
-    const deleteButton = document.createElement("div");
-    deleteButton.classList.add("todo-list__delete-button");
-    deleteButton.addEventListener("click", deleteTask);
-    task.append(deleteButton);
+    task.prepend(init("checkbox"));
+    task.append(init("delete button"));
     task.classList.add("todo-list__task");
     task.dataset.taskid = id;
     list.append(task);
